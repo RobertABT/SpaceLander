aecbdede1069c9f282f2a930ad68eae3b326b626
Robert Buchan-Terrey <rob27@aber.ac.uk>
2018-03-06 13:35:17 +0000

    added IR sensor and got reverting to a 0 point to work

--- Test_Script/Test_Script.ino
+++ Test_Script/Test_Script.ino
@@ -8,93 +8,116 @@ const int stepsPerRevolution = 512;  // change this to fit the number of steps p
 //Circumference=2*pi*r=59.37mm
 //mm per step = 59.37mm/512=0.1159mm/step
 //1metre/7680 steps = 0.1302mm/step
+//A7 is z analog reader
+Stepper stepper_a(stepsPerRevolution, 2, 3, 4, 5);
+Stepper stepper_b(stepsPerRevolution, 8, 9, 10, 11);
+Stepper stepper_c(stepsPerRevolution, 6, 7, 12, 13);
 
-Stepper stepper_a(stepsPerRevolution, 2,3,4,5);
-Stepper stepper_b(stepsPerRevolution, 8,9,10,11);
-Stepper stepper_c(stepsPerRevolution, 6,7,12,13);
+#define END_Z A7
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   stepper_a.setSpeed(40); //slips at 60
   stepper_b.setSpeed(40);
   stepper_c.setSpeed(40);
+  pinMode(END_Z, INPUT);
 }
+
 int stepsa = 0;
 int stepsb = 0;
 int stepsc = 0;
+bool initialZConf = false;
+
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     int input = Serial.read();
-    switch(input){
+    switch (input) {
       case 'x':
-        if(stepsa==7680){
+        if (stepsa == 7680) {
           Serial.println("max a");
           break;
         }
-        Serial.println("clockwise");
+        Serial.println("down");
         stepper_a.step(stepsPerRevolution);
         delay(100);
-        stepsa+=stepsPerRevolution;
+        stepsa += stepsPerRevolution;
         Serial.println(stepsa);
         break;
       case 'y':
-        if(stepsb==7680){
+        if (stepsb == 7680) {
           Serial.println("max b");
           break;
         }
-        Serial.println("clockwise");
+        Serial.println("down");
         stepper_b.step(stepsPerRevolution);
         delay(100);
-        stepsb+=stepsPerRevolution;
+        stepsb += stepsPerRevolution;
         Serial.println(stepsb);
         break;
       case 'z':
-        if(stepsc==7680){
+        if (stepsc == 500) {
+          //if(stepsc==7680){
           Serial.println("max c");
           break;
         }
-        Serial.println("clockwise");
-        stepper_c.step(stepsPerRevolution);
+        Serial.println("down");
+        stepper_c.step(-100);
+        //stepper_c.step(stepsPerRevolution);
         delay(100);
-        stepsc+=stepsPerRevolution;
+        stepsc += 100;
         Serial.println(stepsc);
         break;
       case 'a':
-        if(stepsa==0){
+        if (stepsa == 0) {
           Serial.println("min a");
           break;
         }
-        Serial.println("anti-clockwise");
+        Serial.println("up");
         stepper_a.step(-stepsPerRevolution);
         delay(100);
-        stepsa-=stepsPerRevolution;
+        stepsa -= stepsPerRevolution;
         Serial.println(stepsa);
         break;
       case 'b':
-        /*if(stepsb==0){
+        if (stepsb == 0) {
           Serial.println("min b");
           break;
-        }*/
-        Serial.println("anti-clockwise");
+        }
+        Serial.println("up");
         stepper_b.step(-stepsPerRevolution);
         delay(100);
-        stepsb-=stepsPerRevolution;
+        stepsb -= stepsPerRevolution;
         Serial.println(stepsb);
         break;
       case 'c':
-        if(stepsc==0){
+        Serial.println(analogRead(END_Z));
+        if (stepsc==0) {
           Serial.println("min c");
+          initialZConf == true;
+          stepsc = 0;
           break;
         }
-        Serial.println("anti-clockwise");
-        stepper_c.step(-stepsPerRevolution);
+        Serial.println("up");
+        stepper_c.step(100);
+        //stepper_c.step(-stepsPerRevolution);
         delay(100);
-        stepsc-=stepsPerRevolution;
+        stepsc -= 100;
         Serial.println(stepsc);
         break;
-      
+      case 'q':
+        while (initialZConf == false) {
+          Serial.print("Light Sensor: ");
+          Serial.println(analogRead(END_Z));
+          if (analogRead(END_Z) < 100 ) {
+            Serial.println("min c");
+            initialZConf == true;
+            stepsc = 0;
+            break;
+          }
+          stepper_c.step(10);
+        }
     }
   }
 }
