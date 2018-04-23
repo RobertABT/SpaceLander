f33c3ed49666710fec1e92f0ecfd9a38656383ad
Robert Buchan Terrey <rob27@aber.ac.uk>
2018-04-23 17:05:18 +0100

    Coordinate system is now working, with some errors based on slack

--- serialComm.py
+++ serialComm.py
@@ -7,10 +7,10 @@ ser = serial.Serial('/dev/ttyUSB0', 115200) # Establish the connection on a spec
 pointA = [130,225,0] #these values will likely have to change
 pointB = [0,0,0]
 pointC = [260,0,0]
-initial_guess = (50,50,10)
-#lengthA = 0.1
-#lengthB = 0.1
-#lengthC = 0.1
+initial_guess = (130,110,50)
+initLengthA = 178.0
+initLengthB = 171.0
+initLengthC = 162.0
 initializing = True
 
 def lengthCalc(output):
@@ -19,28 +19,30 @@ def lengthCalc(output):
 	for line in a.split(","):
 		if i==0:
 			global lengthA
-			lengthA = float(line)*0.1159
+			lengthA = initLengthA + (float(line)*0.1159)
 			print lengthA
 		if i==1:
 			global lengthB
-			lengthB = float(line)*0.1159
+			lengthB = initLengthB + (float(line)*0.1159)
 			print lengthB
 		if i==2:
 			global lengthC
-			lengthC = float(line)*0.1159
+			lengthC = initLengthC + (float(line)*0.1159)
 			print lengthC
 		if i>=3:
 			break
 		i+=1
 	print ("lengths A,B,C are ", lengthA, lengthB, lengthC)
 	
+
+
 def equations(guess):
 	landerX, landerY, landerZ = guess
 	
 	return (
-		( landerX - pointA[0] )**2 + ( landerY - pointA[1] )**2 + (landerZ - pointA[2])**2 -(lengthA*0.1159)**2,
-		( landerX - pointB[0] )**2 + ( landerY - pointB[1] )**2 + (landerZ - pointB[2])**2 -(lengthB*0.1159)**2,
-		( landerX - pointC[0] )**2 + ( landerY - pointC[1] )**2 + (landerZ - pointC[2])**2 -(lengthC*0.1159)**2,
+		( landerX - pointA[0] )**2 + ( landerY - pointA[1] )**2 + (landerZ - pointA[2])**2 -(lengthA)**2,
+		( landerX - pointB[0] )**2 + ( landerY - pointB[1] )**2 + (landerZ - pointB[2])**2 -(lengthB)**2,
+		( landerX - pointC[0] )**2 + ( landerY - pointC[1] )**2 + (landerZ - pointC[2])**2 -(lengthC)**2,
 	)
 def sendChar(c):
 	ser.write(c[0]) # send it to the Arduino
@@ -59,11 +61,11 @@ def sendChar(c):
 	lengthCalc(output)
 	
 def reset():
-	while lengthA >=12:
+	while lengthA >=189.58: #this is the begining length + 100steps*0.1159mm/step 
 		sendChar('a')
-	while lengthB >=12:
+	while lengthB >=182.58:
 		sendChar('b')
-	while lengthC >=12:
+	while lengthC >=173.58:
 		sendChar('c')
 
 ####################################################################################################
